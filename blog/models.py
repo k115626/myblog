a9e973ad6a0f0a2efdfb42ee21132d2ea101471e
cjk <chai_jk@163.com>
2021-01-03 16:35:26 +0800

    统计阅读数

--- blog/models.py
+++ blog/models.py
@@ -38,6 +38,11 @@ class Post(models.Model):
     category = models.ForeignKey(Category, verbose_name='分类', on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag, verbose_name='标签', blank=True)
     author = models.ForeignKey(User, verbose_name='作者', on_delete=True)
+    views = models.PositiveIntegerField(default=0, editable=False)
+
+    def increase_views(self):
+        self.views += 1
+        self.save(update_fields=['views'])
 
     def __str__(self):
         return self.title
