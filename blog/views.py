71df4a1c2e300f41ba2580200ec17e9b778de138
cjk <chai_jk@163.com>
2021-01-05 22:17:11 +0800

    go top

--- blog/views.py
+++ blog/views.py
@@ -14,11 +14,7 @@ def entrance(request):
 
 def index(request):
     post_list = Post.objects.all()
-    return render(request, 'blog/index.html', context={
-        'title': '我的博客首页',
-        'welcome': '欢迎访问我的博客首页',
-        'post_list': post_list
-    })
+    return render(request, 'blog/index.html', context={'post_list': post_list})
 
 
 def detail(request, pk):
